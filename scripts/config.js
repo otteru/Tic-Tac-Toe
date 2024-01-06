e4733482c8db3bf2e3efb31e3397fbba0d01fc17
otteru <otter@naver.com>
2024-01-06 11:38:26 +0000

    added day41 content

---
+++ scripts/config.js
@@ -0,0 +1,9 @@
+function openPlayerConfig() {
+	PlayerConfigOverlayElement.style.display = "block";
+	backdropElement.style.display = "block";
+}
+
+function closePlayerConfig() {
+	PlayerConfigOverlayElement.style.display = "none";
+	backdropElement.style.display = "none";
+}
